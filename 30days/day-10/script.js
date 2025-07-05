04bd543eaf3bd29ea79995678eb501ea133ded5e
Wb6 <wb6ya0@gmail.com>
2025-07-05 18:38:52 +0300

    Add files via upload

---
+++ 30days/day-10/script.js
@@ -0,0 +1,49 @@
+const uploadBox = document.querySelector(".upload-box"),
+previewImg = uploadBox.querySelector("img"),
+fileInput = uploadBox.querySelector("input"),
+widthInput = document.querySelector(".width input"),
+HeightInput = document.querySelector(".height input"),
+ratioInput = document.querySelector(".ratio input"),
+downloadBtn = document.querySelector(".download-btn");
+
+let ogImageRatio;
+
+const loadFile = (e) => {
+    const file = e.target.files[0]
+    if (!file) {
+        return
+    }
+    previewImg.src = URL.createObjectURL(file)
+    previewImg.addEventListener("load", () => {
+        widthInput.value = previewImg.naturalWidth;
+        HeightInput.value = previewImg.naturalHeight
+        ogImageRatio = previewImg.naturalWidth / previewImg.naturalHeight
+        document.querySelector(".wrapper").classList.add("active")
+    })
+    console.log(file);
+    
+}
+
+widthInput.addEventListener("keyup", () => {
+    const height = ratioInput.checked ? widthInput.value / ogImageRatio : HeightInput.value;
+    HeightInput.value = Math.floor(height)
+})
+
+HeightInput.addEventListener("keyup", () => {
+    const width = ratioInput.checked ? HeightInput.value * ogImageRatio : widthInput.value;
+    widthInput.value = Math.floor(width)
+})
+
+const resizeAndDownload = () => {
+    const canvas = document.createElement("canvas")
+    const ctx = canvas.getContext("2d")
+
+    canvas.width = widthInput.value
+    canvas.height = HeightInput.value
+
+    ctx.drawImage(previewImg)
+}
+
+downloadBtn.addEventListener("click", resizeAndDownload)
+fileInput.addEventListener("change", loadFile)
+uploadBox.addEventListener("click", () => fileInput.click())
